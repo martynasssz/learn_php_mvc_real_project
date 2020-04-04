b381f2acf4e4cdeaaf76cf28813f818e064d82a0
unknown <j0h@example.com>
2020-04-04 08:54:43 +0300

    Form Validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -7,8 +7,59 @@
 		//   we need to check to see is it post request, is form beeing submited ir we just loading it
 		public function register(){  
 				// Check for post
-			if($_SERVER['REQUEST_METHOD'] == 'POST'){
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){ //we chech is it POST, hen we process the form
 				// Process form	
+					//die('submitted'); check is form submited
+
+				//Sanitize POST data //as strings
+				$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+				// Init data 
+				$data=[
+					'name' => trim($_POST['name']), 
+					'email' => trim($_POST['email']),
+					'password' => trim($_POST['password']),
+					'confirm_password' => trim($_POST['confirm_password']),
+					'name_err' => '',
+					'email_err' => '',
+					'password_err' => '',
+					'confirm_password_err' => ''
+				];	
+
+				// Validate Email
+				if(empty($data['email'])){
+					$data['email_err'] = 'Please enter email';
+				}
+
+				// Validate Name
+				if(empty($data['name'])){
+					$data['name_err'] = 'Please enter name';
+				}
+
+				// Validate Password
+				if(empty($data['password'])){
+					$data['password_err'] = 'Please enter password';
+				} elseif(strlen($data['password']) < 6){ //check password length
+					$data['password_err'] = 'Password must be at least 6 charancters';
+				}
+
+				// Confirm password
+				if(empty($data['confirm_password'])){
+					$data['confirm_password_err'] = 'Please confirm password';
+				} else { 		    //check if matches passwords
+					if($data['password'] != $data['confirm_password']){
+						$data['confirm_password_err'] = 'passwords do not match';
+					}
+				}
+
+				// Make sure errors are apty
+				if(empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+					// Validated
+					die('SUCCESS');
+				} else {
+					// Load view with erros
+					$this->view('users/register', $data); //load view with data
+				}				
 			} else {
 				// Init data 
 				$data =[   //all set to nothing because is blank register form
@@ -27,10 +78,40 @@
 			}
 		}
 		
-		public function login(){  
+		public function login(){ //we chech is it POST, hen we process the form 
 				// Check for post
-			if($_SERVER['REQUEST_METHOD'] == 'POST'){
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){ //we chech is it POST, hen we process the form
 				// Process form	
+				//die('submitted'); //check is form submited
+				//Sanitize POST data //as strings
+				$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+				// Init data 
+				$data=[
+					'email' => trim($_POST['email']),
+					'password' => trim($_POST['password']),									
+					'email_err' => '',
+					'password_err' => ''
+				];	
+
+				// Validate Email
+				if(empty($data['email'])){
+					$data['email_err'] = 'Please enter email';
+				}
+
+				// Validate password
+				if(empty($data['password'])){
+					$data['password_err'] = 'Please enter password';
+				}
+
+				// Make sure errors are empty
+				if(empty($data['email_err']) && empty($data['password_err'])) {
+					// Validated
+					die('SUCCESS');
+				} else {
+					// Load view with erros
+					$this->view('users/login', $data); //load view with data
+				}	
 			} else {
 				// Init data 
 				$data =[   //all set to nothing because is blank register form
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -4,7 +4,7 @@
 			<div class="card card-body bg-light mt-5">
 				<h2>Login</h2>
 				<p>Please fill in your credentials to log in</p>
-				<form action="<?php echo URLROOT; ?>/users/loggin" method="post">
+				<form action="<?php echo URLROOT; ?>/users/login" method="post">
 					
 					<div class="form-group">
 						<label for="email">Email: <sup>*</sup></label>

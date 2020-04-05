40f95ed45144aa9b4add7ee47e1a880823cd4f3d
unknown <j0h@example.com>
2020-04-05 18:15:11 +0300

    Inserting posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -22,14 +22,55 @@
 		}
 
 		public function add(){
+			if($_SERVER['REQUEST_METHOD']=='POST'){
+
+				// Sanitize POST arra
+				$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+				$data = [
+					'title' => trim($_POST['title']),
+					'body' => trim($_POST['body']),
+					'user_id' => $_SESSION['user_id'],  //user_id comes form a session
+					'title_err' => '',
+					'body_err' =>''
+				];
+
+				// Validate title
+				if(empty($data['title'])){
+					$data['title_err'] = 'Please enter title';
+				}
+
+				// Validate body
+				if(empty($data['body'])){
+					$data['body_err'] = 'Please enter body text';
+				}
+
+				// Make sure no errors
+				if(empty($data['title_err']) && empty($data['body_err'])){//if those things are true
+					// Validated
+					if($this->postModel->addPost($data)){
+						flash('post_message', 'Post Added');
+						redirect('posts');
+					} else {
+						die('Somethig went wrong');
+					}
+
+				} else {
+					// Load view with errors
+					$this->view('posts/add', $data);
+				}
+			
+			} else {
+				$data = [
+					'title' => '',
+					'body' =>''
+				];
+			}
+
 			$data = [
 				'title' => '',
 				'body' => ''
 			];
 
 			$this->view('posts/add', $data);
-
-
-
 		}
 	}
--- app/views/posts/add.php
+++ app/views/posts/add.php
@@ -3,7 +3,7 @@
 			<div class="card card-body bg-light mt-5">
 				<h2>Add post</h2>
 				<p>Crate a post with this form</p>
-				<form action="<?php echo URLROOT; ?>/users/posts/add" method="post">
+				<form action="<?php echo URLROOT; ?>/posts/add" method="post">
 					<div class="form-group">
 						<label for="title">Title<sup>*</sup></label>
 						<input type="text" name="title" class="form-control form-control-lg <?php echo (!empty($data['title_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['title']; ?>"><!-- not empty means is name_err-->
@@ -15,7 +15,7 @@
 						<textarea name="body" class="form-control form-control-lg <?php echo (!empty($data['body_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['body'] ; ?></textarea><!-- not empty means is name_err-->
 							<span class="invalid-feedback"><?php echo $data['body_err']; ?></span><!-- we want display error and don't keep box red-->
 					</div>	
-					<input type="submit" class="btn btn-succsess" value="Sumbit">					
+					<input type="submit" class="btn btn-success" value="Sumbit">					
 				</form>
 			</div>
 		
